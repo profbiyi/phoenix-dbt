9df4cc6a2e4fcc944f21939cdb4d5d9c0f0b387e
Patience Enyinwa <Cheezy.ep@gmail.com>
2025-07-01 16:39:17 +0000

    uadded cat_id to dim_products

--- models/mart/dim_product.sql
+++ models/mart/dim_product.sql
@@ -5,7 +5,8 @@ with distinct_products as (
     select distinct
         {{ dbt_utils.generate_surrogate_key(['product_category', 'product_name']) }} as product_id,
         product_name,
-        product_category
+        product_category,
+         {{ dbt_utils.generate_surrogate_key(['product_category']) }} as category_id
     from {{ ref('stg_customer_orders') }}
 
 )
